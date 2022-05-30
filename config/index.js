6379bed71c00ce7efcfea025f7300456e95ff8f3
Pablo <pablo.galvan@mercadolibre.com>
2022-05-30 14:06:07 -0300

    first

---
+++ config/index.js
@@ -0,0 +1,40 @@
+module.exports = {
+  branches: [
+    {
+      name: 'Bernal',
+      id: '36'
+    },
+    {
+      name: 'Solano',
+      id: '59'
+    },
+    {
+      name: 'Estevan Echeverria',
+      id: '33'
+    },
+    {
+      name: 'Berisso',
+      id: '42'
+    },
+    {
+      name: 'Berazategui',
+      id: '32'
+    },
+    {
+      name: 'Lomas de Zamora',
+      id: '2'
+    },
+    {
+      name: 'Lanus',
+      id: '1'
+    },
+    {
+      name: 'La Plata',
+      id: '40'
+    },
+    {
+      name: 'Castelli',
+      id: '38'
+    }
+  ]
+};
